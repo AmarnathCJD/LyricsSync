771a1b91b9645c515c73c8ac4b0f23559821992b
AmarnathCJD <amarnathcharichilil@gmail.com>
2025-01-17 23:51:33 +0530

    make lyrics pritnitn smooth, optimise code, add colors

--- songify.py
+++ songify.py
@@ -5,8 +5,10 @@ import time
 import subprocess
 import signal
 import re
+import random
 
-active_duration = 0.0
+start_time = 0.0
+
 
 def get_embedded_tags(filename):
     try:
@@ -28,7 +30,11 @@ def parse_lyrics(lyrics_text):
 
     return lyrics
 
+
 def play_audio(filename):
+    global start_time
+    start_time = time.time()
+
     return subprocess.Popen(
         ["ffplay", "-nodisp", "-autoexit", filename],
         stdout=subprocess.DEVNULL,
@@ -37,9 +43,8 @@ def play_audio(filename):
 
 
 def display_lyrics(lyrics, duration):
-    global active_duration
     for timestamp, lyric in lyrics:
-        while active_duration < timestamp:
+        while time.time() - start_time < timestamp:
             time.sleep(0.1)
         try:
             write_lyrics(
@@ -49,25 +54,35 @@ def display_lyrics(lyrics, duration):
             write_lyrics(lyric, duration - timestamp)
 
 
+colors = {
+    "violet": "\033[95m",
+    "purple": "\033[35m",
+    "navy": "\033[34m",
+    "blue": "\033[94m",
+    "green": "\033[92m",
+    "white": "\033[97m",
+}
+
+def get_color():
+    return random.choice(list(colors.values()))
+
+color = get_color()
+
 def write_lyrics(content, in_time):
     if not content or in_time <= 0:
         return
 
-    delay = in_time / len(content)
+    delay = 0.10  # in_time / len(content)
 
+    print(color, end="")
     for char in content:
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(delay)
 
+    print("\033[0m", end="")
+
     print()
-
-
-def update_active_duration():
-    global active_duration
-    while active_duration < duration:
-        time.sleep(1)
-        active_duration += 1
 
 
 def handle_exit(signum, frame):
@@ -86,8 +101,6 @@ if __name__ == "__main__":
     print("Lyrics loaded successfully!\n\n")
 
     audio_process = play_audio(FILENAME)
-
-    threading.Thread(target=update_active_duration, daemon=True).start()
     threading.Thread(
         target=display_lyrics, args=(lyrics, duration), daemon=True
     ).start()
